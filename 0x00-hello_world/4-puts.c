a698136e61d6fccf68e001f5a2a0458b467c43fe
Paraclete12 <oliverparaclete@gmail.com>
2023-02-17 05:01:11 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0 (Sucess)
+ * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0)
+	return(0);
 }
