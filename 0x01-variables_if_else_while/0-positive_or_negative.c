69b6e0394a54605aeb2dcc60023f062ffc5531df
Paraclete12 <oliverparaclete@gmail.com>
2023-02-17 16:00:49 -0800

    ++ or --

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n = 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
@@ -25,6 +25,5 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-
 	return (0);
-}		
+}
