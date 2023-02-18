0ebb03a727c77d6a5ab89c3402ed0e3a139e2a52
Paraclete12 <oliverparaclete@gmail.com>
2023-02-17 17:49:56 -0800

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,16 @@
-
+#include <stdio>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+		}
+	return (0);
+}
+	
