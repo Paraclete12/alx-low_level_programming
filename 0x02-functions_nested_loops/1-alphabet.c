b6ba926f642e44abbae7ec6de26eb7df458b716c
Paraclete12 <oliverparaclete@gmail.com>
2023-02-21 14:33:49 -0800

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Alphabet in lowercase
+ *
+ *Return:0
+ *
+ */
+void print_alphabet(void)
+{
+	char r;
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+		_putchar(r);
+	}
+	_putchar('\n');
+	return (0);
+}
