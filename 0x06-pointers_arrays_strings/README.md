9a5bf77f990778ba4c2552c6eca7f2824cd1d8b9
Paraclete12 <oliverparaclete@gmail.com>
2023-03-01 13:05:27 -0800

    concatenate function

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+06 pointers_arrays_strings
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void print_buffer(char *b, int size);
+int _putchar(char C);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+char *rot13(char *);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+
+#endif
