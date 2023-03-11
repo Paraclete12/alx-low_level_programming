21f8fbf25efaeebd6ce909e79c56a75e9c9c1d9e
Paraclete12 <oliverparaclete@gmail.com>
2023-03-11 14:12:31 -0800

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*#include <ctype.h>*/
+/**
+ * main - program that adds integers
+ * @argc: argument constant parameter
+ * @argv: argument vector parameter
+ * Return: 0 or 1
+ * Date: 11-03-2023
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+	}
+		sum += atoi(argv[i]);
+	}
+		printf("%d\n", sum);
+	return (0);
+}
+
