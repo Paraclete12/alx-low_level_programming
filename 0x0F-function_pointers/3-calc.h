6fed3af5adc9d058631b7fb0024cb7be737af8e8
Paraclete12 <oliverparaclete@gmail.com>
2023-03-23 01:46:43 -0700

    function_pointer updates

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,6 @@
-#ifndef CALC_H
-#define CALC_H
-#include <stddef.h>
+#ifndef _CALC_H_
+#define _CALC_H_
+
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -11,8 +11,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -69,3 +69,4 @@ int op_mod(int a, int b)
 	}
 	return (a % b);
 }
+
