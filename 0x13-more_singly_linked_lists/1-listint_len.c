cd01c49e3af5a0312067298c55cb60044467ba6d
Paraclete12 <oliverparaclete@gmail.com>
2023-04-04 16:24:11 -0700

    task 1 amd 2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - this function prints number of the element.
+ * @h: pointer to new node.
+ * Return: Pointer to Node.
+ * Date: 04-04-2023
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t index = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		index++;
+	}
+	return (index);
+}
